547f7fe0c5d84cc1c8fb88b0993bd9fa836a380d
bgalderisi <34248613+bgalderisi@users.noreply.github.com>
2025-12-13 22:55:08 -0500

    Update node_helper.js
    enhance log

--- node_helper.js
+++ node_helper.js
@@ -577,7 +577,7 @@ module.exports = NodeHelper.create({
 
   initServer(port) {
     if (this.server) return;
-    Log.log(`initServer: port: ${port}`);
+    Log.log(`MMM-Chores - initServer - port: ${port}`);
     const self = this;
     const app  = express();
 
